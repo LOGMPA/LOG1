f6a40e9537e5f598a6cc8477da8238fbefcc2763
LOGMPA <mpamarcelamarques@gmail.com>
2025-11-14 19:20:45 -0300

    Delete src directory

--- src/components/ui/input.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export function Input({ className = "", ...props }) {
-  return <input className={"w-full rounded-lg border border-gray-300 px-3 py-2 text-sm " + className} {...props} />;
-}
